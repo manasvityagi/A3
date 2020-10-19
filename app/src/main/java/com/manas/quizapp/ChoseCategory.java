d1b11b141af4f88d6b5d11df22077f722272bcc1
manastyagi <manasvi.tyagi@gmail.com>
2020-10-20 03:37:56 +1300

    update

--- app/src/main/java/com/manas/quizapp/ChoseCategory.java
+++ app/src/main/java/com/manas/quizapp/ChoseCategory.java
@@ -2,7 +2,6 @@ package com.manas.quizapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,7 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class ChoseCategory extends AppCompatActivity {
 
     private TextView mTextView;
-    Button btnCatAll;
+    Button btnCat1;
     Button btnCat2;
     Button btnCat3;
     Button btnCat4;
@@ -27,14 +26,14 @@ public class ChoseCategory extends AppCompatActivity {
 
 
         //Multi,div,add, sub
-        btnCatAll = findViewById(R.id.cat_1);
+        btnCat1 = findViewById(R.id.cat_1);
         btnCat2 = findViewById(R.id.cat_2);
         btnCat3 = findViewById(R.id.cat_3);
         btnCat4 = findViewById(R.id.cat_4);
         btnCat5 = findViewById(R.id.cat_5);
         btnCat6 = findViewById(R.id.cat_6);
 
-        btnCatAll.setOnClickListener(new View.OnClickListener() {
+        btnCat1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 ChoseCategory = "mixed";
