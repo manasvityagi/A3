c67115cda96b8581846f8592ab9b805f829248a3
manastyagi <manasvi.tyagi@gmail.com>
2020-10-20 17:44:03 +1300

    update

--- app/src/main/java/com/manas/quizapp/QuizActivity.java
+++ app/src/main/java/com/manas/quizapp/QuizActivity.java
@@ -2,13 +2,24 @@ package com.manas.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 public class QuizActivity extends AppCompatActivity {
+    SQLiteDatabase dbHandler = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initiateDatabase();
+        
         setContentView(R.layout.activity_quiz);
+    }
+
+    private void initiateDatabase() {
+
+        // Open database handler using our own specialized CustomerDatabaseHelper
+        QuizDAO helper = new QuizDAO(getApplicationContext());
+        dbHandler = helper.getWritableDatabase();
     }
 }
