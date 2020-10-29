2bd5e532cb1b026e0512a5ab86dcfe26ea7b9323
manastyagi <manasvi.tyagi@gmail.com>
2020-10-30 09:46:39 +1300

    check in

--- app/src/main/java/com/manas/quizapp/QuizActivity.java
+++ app/src/main/java/com/manas/quizapp/QuizActivity.java
@@ -2,6 +2,7 @@ package com.manas.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
@@ -26,38 +27,49 @@ public class QuizActivity extends AppCompatActivity {
     RadioButton radioOption4;
     RadioButton selectedRadioButton;
     Button submitButton;
+    int quizQuesLength;
+    private Integer currentScore = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        loadQuestionsFromDB();
+        this.questionList = getQuestionList();
         setupUI();
 
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                // get selected radio button from radioGroup
+                // get selected radio button from radioGroup~
                 int selectedId = quesRadioGroup.getCheckedRadioButtonId();
                 // find the radiobutton by returned id
                 selectedRadioButton = (RadioButton) findViewById(selectedId);
                 Toast.makeText(QuizActivity.this,
                         selectedRadioButton.getText(), Toast.LENGTH_SHORT).show();
                 Log.e("app",  selectedRadioButton.getText().toString());
-                loadNextQuestion(questionList.get(currentQuestionPointer));
+                loadQuestionOnUI(questionList.get(currentQuestionPointer));
                 currentQuestionPointer+=1;
 
+                if(currentQuestionPointer == quizQuesLength){
+                    Intent intent = new Intent(QuizActivity.this, FinalScore.class);
+                    intent.putExtra("score", currentScore);
+                    startActivity(intent);
+                }
             }
         });
 
     }
 
-    private void loadNextQuestion(QuizQuestionsModel individualQuestions){
-        Log.e("app", "Question Statement " + individualQuestions.getQuestionStatement());
-        loadNextQuestion(questionList.get(currentQuestionPointer));
-        currentQuestionPointer+=1;
+    @Override
+    protected void onPostResume() {
+        Toast.makeText(QuizActivity.this,
+                "Resumed!!!!!!!!!!", Toast.LENGTH_SHORT).show();
+        super.onPostResume();
+    }
 
+    private void loadQuestionOnUI(QuizQuestionsModel individualQuestions){
+        Log.e("app", "Question Statement " + individualQuestions.getQuestionStatement());
         questionStatementField.setText(individualQuestions.getQuestionStatement());
         radioOption1.setText(individualQuestions.getOption1());
         radioOption2.setText(individualQuestions.getOption2());
@@ -68,25 +80,18 @@ public class QuizActivity extends AppCompatActivity {
     private List<QuizQuestionsModel> getQuestionList() {
         Bundle bundle = getIntent().getExtras();
         String categoryPassedToThis = getIntent().getStringExtra("category");
-         categoryPassedToThis = "courtesy";
-        Integer quizLength = bundle.getInt("quiz_length");
-        Log.e("app", String.valueOf(quizLength) + " category " + categoryPassedToThis);
-
-        Toast.makeText(this, categoryPassedToThis + " : " + String.valueOf(quizLength), Toast.LENGTH_LONG).show();
+        categoryPassedToThis = "courtesy";
+        this.quizQuesLength = bundle.getInt("quiz_length");
 
         // Open database handler using our own specialized CustomerDatabaseHelper
         QuizDAO helper = new QuizDAO(getApplicationContext());
         List<QuizQuestionsModel> questionList = helper.getQuestions(categoryPassedToThis, quizLength);
 
-        for (QuizQuestionsModel individualQuestions : questionList) {
-            Log.e("app", "Question Statement " + individualQuestions.getQuestionStatement());
-        }
         return questionList;
     }
 
     private void setupUI() {
         setContentView(R.layout.activity_quiz);
-
         questionStatementField = (TextView) findViewById(R.id.QuestionStatement);
         radioOption1 = (RadioButton) findViewById(R.id.radioButton1);
         radioOption2 = (RadioButton) findViewById(R.id.radioButton2);
@@ -95,12 +100,9 @@ public class QuizActivity extends AppCompatActivity {
         submitButton = (Button) findViewById(R.id.submit_answer);
         quesRadioGroup = findViewById(R.id.radioGroup);
         //Load First Question
-        loadNextQuestion(questionList.get(0));
+        loadQuestionOnUI(questionList.get(0));
         currentQuestionPointer=1;
     }
 
-    private void loadQuestionsFromDB() {
-        this.questionList = getQuestionList();
-    }
 
 }
